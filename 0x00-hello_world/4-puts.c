84d1c44443383302e100487ed33dbc94533c236b
Abigaelchemtai <abigaelchemtai8@gmail.com>
2023-02-16 12:34:14 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+/** Program to print a line with puts functions
+ * Return:0 (success)
+ * */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+	)
