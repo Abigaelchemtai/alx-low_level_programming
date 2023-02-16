251f41f6f693421f12c3ebfec548c1542264a732
Abigaelchemtai <118007085+Abigaelchemtai@users.noreply.github.com>
2023-02-17 01:49:41 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Prints a statement to the standard errr
  * Return: 1
  */
 int main(void)
 {
-	fprintf(stderr, "\"and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "\"and that piece of art is useful\" -Dora Korpar, 2015-1-19\n");
 	return (1);
 }
