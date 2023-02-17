ef23f27dd3c017fd91716212bae3794e52aca33f
Abigaelchemtai <118007085+Abigaelchemtai@users.noreply.github.com>
2023-02-18 00:51:36 +0300

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,10 +9,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if ( n>0)
-	printf("%d is possible \n", n);
-	else if ( == 0)
-	  printf("%d is zero \n", n);
+	printf("%d is possible. \n", n);
+	else if (n == 0)
+	  printf("%d is zero. \n", n);
 	else
-	 printf("%d is zero \n", n);
+	 printf("%d is zero. \n", n);
 	return (0);
 }
