13f643ae93cfa933877edd8f53a59890fbc22a38
Abigaelchemtai <118007085+Abigaelchemtai@users.noreply.github.com>
2023-02-18 02:59:21 +0300

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,11 +13,13 @@ int last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 last_digit =n % 10;
+printf("The last digit of %d is %d", n, last_digit
 if (last_digit > 5)
-printf("The last digit of %d is %d greater than 5\n", n);
+printf("and is greater than 5");
 else if (last_digit == 0)
-printf("The last digit of %d is %d  0\n", n);
+printf("and is 0");
 else if (last_digit < 6 && last_digit !=0)
-printf("The last digit of %d is %d is less than 6 and not 0\n", n);
+printf("and less than 6 and not 0");
+printf("\n");
 return (0);
 }
