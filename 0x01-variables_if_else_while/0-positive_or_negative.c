d3db927e4993ad06e4ff46021b1fc82db57ee0d1
Abigaelchemtai <abigaelchemtai8@gmail.com>
2023-02-17 13:11:15 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,26 @@
-gcc -n $RAND_MAX 
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -Determine positive, negative and zero
+ * Return: 0
+ */
+int main(void)
+{
+	int n
+	srand(time (0));
+	n=rand()_RAND_MAX/2;
+	if(n>0)
+	{
+		printf("%d is possible \n" ,n);
+	}
+	else if(n==0)
+	{
+                printf("%d is zero \n" ,n);
+	}
+	else
+	{
+		printf("%d is zero \n" ,n);
+	}
+	return (0);
+}
