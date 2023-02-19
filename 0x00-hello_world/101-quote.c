16562e1254fe4281400a8768bc8df739ce46335c
Abigaelchemtai <abigaelchemtai8@gmail.com>
2023-02-19 12:46:57 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,24 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 0
+ * main - A C program that prints a line to the standard error
+ * Return: 1 (Success)
  */
 int main(void)
 {
-int i;
-
-for (i = 0; i < 16; i++)
-{
-if (i < 10)
-{
-putchar(i + '0');
-}
-else
-{
-putchar(i - 10 + 'a');
-}
-}
-putchar('\n');
-return (0);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
