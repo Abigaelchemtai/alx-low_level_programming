eda539b3ecf5cf08c609b442a9f406082afb7d53
Abigaelchemtai <abigaelchemtai8@gmail.com>
2023-02-22 05:23:02 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,14 +6,16 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
-		_putchar('1');
-	}
-	else
-	{
-		_putchar('0');
-	}
+	int r;
+
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
 	_putchar('\n');
 	return (0);
 }
