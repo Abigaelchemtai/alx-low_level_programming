3e30dbf209cf95bef4aa0ebbddd93b35cbe2bab8
Abigaelchemtai <abigaelchemtai8@gmail.com>
2023-02-27 08:42:27 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: value of the string character
+ * Return: void
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
