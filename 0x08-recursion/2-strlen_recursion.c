3c2a53dc989bf3de43a4d7b43814e045710dd932
Abigaelchemtai <abigaelchemtai8@gmail.com>
2023-03-09 06:18:13 -0800

    ./2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Returns length of a string
+ * @s: The string to be measured
+ * Return:Lngth of a string
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+	return(length);
+}
