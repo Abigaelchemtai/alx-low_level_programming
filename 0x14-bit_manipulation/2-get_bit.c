b1be334b2df4a15eea182d61ef69fb44e945e05d
Abigaelchemtai <abigaelchemtai8@gmail.com>
2023-04-10 10:31:14 -0700

    ./c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_bit - Function that returns the value of a bit at an index in a decimal number
+ * @n: The number to search
+ * @index: The index of the bit
+ * Return: The value of the bit at index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int value;
+
+	if (index > 63)
+		return (-1);
+	value = (n >> index) & 1;
+	return (value);
+}
