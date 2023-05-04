f9ad9771820bfd7aaf831d6ac91776a678932d9a
Abigael chemtai <abigaelchemtai8@gmail.com>
2023-05-04 11:24:24 -0700

    Or-Task1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Function that converts a binary number to unsigned int
+ * @b: The string with the binary number
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int w;
+	unsigned int decimal_value = 0;
+
+	if (!w)
+		return (0);/**if the statement is true return 0 **/
+
+	for (w = 0; b[w]; w++)
+	{
+		if (b[w] < '0' || b[w] > '1')
+			return (0);
+		decimal_value = 2 * decimal_value + (b[w] - '0');
+	}
+	return (dec_val);/** else if statement is false return the decimal value*/
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,6 @@
+0x14. C - Bit manipulation
+Author: Abigael Chemtai
+Cohort 13
+The Feynman Learning Technique
+The Feynman Technique is the best way to supercharge your learning. And it works no matter the subject. Devised by Nobel Prize-winning physicist Richard Feynman, it leverages the power of teaching for better learning.
+Learning doesn’t happen from skimming through a book or remembering enough to pass a test.
